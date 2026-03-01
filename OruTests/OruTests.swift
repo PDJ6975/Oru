c9889e91d93eb2214401ce4aee6c4843edf5b05e
PDJ6975 <antrodcal@alum.us.es>
2026-03-01 10:16:49 +0100

    Initial Commit

---
+++ OruTests/OruTests.swift
@@ -0,0 +1,17 @@
+//
+//  OruTests.swift
+//  OruTests
+//
+//  Created by Antonio Rodríguez Calderón on 1/3/26.
+//
+
+import Testing
+@testable import Oru
+
+struct OruTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
